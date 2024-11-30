9582c4b02508c4d161f32ce446307d9ee45a4a36
jerkokosic <jerkokosic1@gmail.com>
2024-11-30 10:35:27 +0100

    Source and config init commit

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright Three Brown Bears
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Three Brown Bears
+
+#pragma once
+
+#include "CoreMinimal.h"
+
