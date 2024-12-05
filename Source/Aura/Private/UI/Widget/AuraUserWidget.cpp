04f2e211aa4279788fc447ce0ad13246b6108367
jerkokosic <jerkokosic1@gmail.com>
2024-12-05 19:48:48 +0100

    adding aura user widget and widget controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Three Brown Bears
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright Three Brown Bears
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
